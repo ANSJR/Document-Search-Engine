7a30b20c483fc7eb8a8fd2e6d410abd9ddd4fd2a
ANSJR <103015121+ANSJR@users.noreply.github.com>
2025-11-01 20:33:50 -0700

    Prefix search fetures still under development, TST build will prefix fetures of TST still buggy

--- include/TernarySearchTree.h
+++ include/TernarySearchTree.h
@@ -1,14 +1,35 @@
 
 #ifndef TERNARYSEARCHTREE_H
 #define TERNARYSEARCHTREE_H
+#include <string>
+#include <vector>
 
-class TernarySearchTree {
+class TreeNode {
 public:
-    TernarySearchTree() = default;
-    ~TernarySearchTree() = default;
+    char data;
+    bool marked = false;
+    TreeNode* lChild;
+    TreeNode* mChild;
+    TreeNode* rChild;
+    TreeNode(char data);
+    TreeNode(bool marked);
+    TreeNode(char data, bool marked);
+    TreeNode(char data, TreeNode* lChild, TreeNode* mChild, TreeNode* rChild);
+};
+class TernarySearchTree {
+private:
+    TreeNode* root;
 
-    void insert(const std::string& key) {}
-    bool search(const std::string& key) const { return false; }
+    void traverseTST(TreeNode* root, char* buffer, int depth);
+    TreeNode* insertHelper(TreeNode* curr, const std::string& key, int keyIndex);
+public:
+    TernarySearchTree();
+    ~TernarySearchTree();
+
+    void insert(const std::string& key);
+    void prefixHelper(TreeNode* node, std::string current, std::vector<std::string>& results) const;
+    std::vector<std::string> prefixSearch(const std::string& prefix) const;
+    void printTST();
 };
 
 #endif 
---
+++ src/TernarySearchTree.cpp
@@ -0,0 +1,101 @@
+
+#include <../include/TernarySearchTree.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+
+
+TreeNode::TreeNode(char data) : data(data), marked(false), lChild(nullptr), mChild(nullptr), rChild(nullptr) {}
+TreeNode::TreeNode(bool marked) : marked(marked), lChild(nullptr), mChild(nullptr), rChild(nullptr) {}
+TreeNode::TreeNode(char data, bool marked) : data(data), marked(false), lChild(nullptr), mChild(nullptr), rChild(nullptr) {}
+TreeNode::TreeNode(char data, TreeNode* lChild, TreeNode* mChild, TreeNode* rChild) : data(data), marked(false), lChild(lChild), mChild(mChild), rChild(rChild) {}
+
+
+TernarySearchTree::TernarySearchTree() {
+    root = nullptr;
+}
+TernarySearchTree::~TernarySearchTree() = default;
+
+void TernarySearchTree::insert(const std::string& key) {
+    if (key.empty()) {return;}
+    root = insertHelper(root, key, 0);
+}
+TreeNode* TernarySearchTree::insertHelper(TreeNode* curr, const std::string& key, int keyIndex) {
+    if (curr == nullptr) {
+        curr = new TreeNode(key[keyIndex]);
+    }
+    if (curr->data > key[keyIndex]) {
+        curr->lChild = insertHelper(curr->lChild, key, keyIndex);
+    }
+    else if (curr->data < key[keyIndex]) {
+        curr->rChild = insertHelper(curr->rChild, key, keyIndex);
+    }
+    else { 
+        if (keyIndex + 1 < key.size())
+            curr->mChild = insertHelper(curr->mChild, key, keyIndex + 1);
+        else
+            curr->marked = true;
+    }
+    return curr;
+}
+
+void TernarySearchTree::prefixHelper(TreeNode* node, std::string current, std::vector<std::string>& results) const {
+    if (!node) return;
+    current.push_back(node->data);
+    if (node->marked)
+        results.push_back(current);
+    prefixHelper(node->lChild, current, results);
+    prefixHelper(node->mChild, current, results);
+    prefixHelper(node->rChild, current, results);
+}
+
+
+std::vector<std::string> TernarySearchTree::prefixSearch(const std::string& prefix) const {
+    std::vector<std::string> results;
+    if (!root || prefix.empty()) return results;
+
+    TreeNode* node = root;
+    int index = 0;
+
+    while (node) {
+        char c = prefix[index];
+        if (c < node->data)
+            node = node->lChild;
+        else if (c > node->data)
+            node = node->rChild;
+        else { // match
+            index++;
+            if (index == prefix.size())
+                break;  // found last prefix char
+            node = node->mChild;
+        }
+    }
+
+    if (!node) return results;
+    std::string current = prefix.substr(0, prefix.size() - 1);
+
+    prefixHelper(node, current, results);
+    return results;
+}
+
+
+void TernarySearchTree::traverseTST(TreeNode* root, char* buffer, int depth)
+{
+    if (root) {
+        traverseTST(root->lChild, buffer, depth);
+        buffer[depth] = root->data;
+        if (root->marked) {
+            buffer[depth + 1] = '\0';
+            std::cout << buffer << std::endl;
+        }
+        traverseTST(root->mChild, buffer, depth + 1);
+        traverseTST(root->rChild, buffer, depth);
+    }
+}
+void TernarySearchTree::printTST()
+{
+    int MAX = 200;
+    char buffer[MAX];
+    traverseTST(root, buffer, 0);
+}
